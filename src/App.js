0a20d1c6b547d60f0b663a1767404793fd116cfd
devilla <dev.koold@gmail.com>
2020-08-14 21:02:15 +0530

    Updates

--- src/App.js
+++ src/App.js
@@ -14,12 +14,31 @@ class App extends React.Component {
       result: 0,
       showResult: false,
       classicDice: true,
-      hashDice: false
+      hashDice: false,
+      validated: false
     }
   }
 
+  handleSubmit = (event) => {
+
+    const { ServerSeed, ClientSeed, Nonce, classicDice, hashDice } = this.state;
+
+    const form = event.currentTarget;
+    if (form.checkValidity() === false || ServerSeed === '' || ClientSeed === '') {
+      event.preventDefault();
+      event.stopPropagation();
+      this.setState({ validated: true })
+    }
+    else
+      this.setState({
+        result: classicDice ? roll(`${ServerSeed}:${ClientSeed}:${Nonce}`) : hashDice ? getRoll(`${ServerSeed}${ClientSeed}${Nonce}`) : 0,
+        showResult: true,
+        validated: true
+      })
+  }
+
   render() {
-    const { ServerSeed, ClientSeed, Nonce, result, showResult, classicDice, hashDice } = this.state;
+    const { ServerSeed, ClientSeed, Nonce, result, showResult, classicDice, hashDice, validated } = this.state;
     return (
       <div className="container pt-5">
         <Media>
@@ -37,49 +56,53 @@ class App extends React.Component {
         </Media>
         <Row className="mt-5 ml-3">
           <Button variant={classicDice ? 'primary' : 'light'} onClick={() => {
-            this.setState({ classicDice: true, hashDice: false, showResult: false })
+            this.setState({
+              classicDice: true, hashDice: false, showResult: false, ServerSeed: '',
+              ClientSeed: '', Nonce: 0, validated: false
+            })
           }} >
             Classic Dice
             </Button>
           <Button variant={hashDice ? 'primary' : 'light'} className="ml-2" onClick={() => {
-            this.setState({ hashDice: true, classicDice: false, showResult: false })
+            this.setState({
+              hashDice: true, classicDice: false, showResult: false, ServerSeed: '',
+              ClientSeed: '', Nonce: 0, validated: false
+            })
           }} >
             Hash Dice
             </Button>
         </Row>
-        <Form className="mt-5 col-md-5">
+        <Form className="mt-5 col-md-5" noValidate validated={validated} >
           <Form.Group controlId="formBasicServerSeed">
             <Form.Label>Server Seed</Form.Label>
-            <Form.Control type="ServerSeed" placeholder="Enter Server Seed" value={ServerSeed} onChange={(e) => {
+            <Form.Control type="text" required placeholder="Enter Server Seed" value={ServerSeed} onChange={(e) => {
               this.setState({ ServerSeed: e.target.value })
             }} />
-            <Form.Text className="text-muted">
-              We'll never share your Server Seed with anyone else.
-          </Form.Text>
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid Server Seed.
+          </Form.Control.Feedback>
           </Form.Group>
 
           <Form.Group controlId="formBasicClientSeed">
             <Form.Label>Client Seed</Form.Label>
-            <Form.Control type="ClientSeed" placeholder="Enter Client Seed" value={ClientSeed} onChange={(e) => {
+            <Form.Control type="text" required placeholder="Enter Client Seed" value={ClientSeed} onChange={(e) => {
               this.setState({ ClientSeed: e.target.value })
             }} />
-            <Form.Text className="text-muted">
-              We'll never share your Client Seed with anyone else.
-          </Form.Text>
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid Client Seed.
+          </Form.Control.Feedback>
           </Form.Group>
 
           <Form.Group controlId="formBasicNonce">
             <Form.Label>Nonce</Form.Label>
-            <Form.Control type="number" placeholder="Nonce" value={Nonce} onChange={(e) => {
+            <Form.Control type="number" required placeholder="Nonce" value={Nonce} onChange={(e) => {
               this.setState({ Nonce: e.target.value })
             }} />
           </Form.Group>
-          <Button variant="primary" type="button" onClick={() => {
-            this.setState({ result: classicDice ? roll(`${ServerSeed}:${ClientSeed}:${Nonce}`) : hashDice ? getRoll(`${ServerSeed}${ClientSeed}${Nonce}`) : 0, showResult: true })
-          }}>
+          <Button variant="primary" type="button" onClick={this.handleSubmit}>
             Submit
         </Button>
-          {showResult && <Alert className="mt-3" variant="success">
+          {showResult && validated && <Alert className="mt-3" variant="success">
             The result is : {result} !
         </Alert>
           }
